86b838909f8f2d10e025d4735696e42b58691a91
Adithya Ajith <adithya@satyaloka.org>
2025-04-25 18:26:51 +0530

    Condense navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,6 @@ export default function Header() {
     <div className="w-full flex flex-col">
       <div className="w-full flex flex-row items-center justify-between">
         <Title />
-        <div className="flex items-center gap-4 text-lg d-respira font-bold">
-          <Button title="HOME" />
-          <Button title="ABOUT" />
-        </div>
       </div>
       <Navbar />
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,13 @@ export default function Navbar() {
   return (
     <div className="flex flex-col gap-2">
       <Separator />
-      <div className="w-full flex flex-row items-center justify-between d-respira font-extrabold">
+      <div className="w-full flex flex-row items-center gap-4 d-respira font-extrabold justify-between">
+        <p>HOME</p>
+        <div className="w-2 aspect-square bg-[var(--damask)] rounded-full" />
+        <p>ABOUT</p>
+        <div className="w-2 aspect-square bg-[var(--damask)] rounded-full" />
         <p>ARTWORK</p>
+        <div className="w-2 aspect-square bg-[var(--damask)] rounded-full" />
         <p>ARTICLES</p>
       </div>
       <Separator />
