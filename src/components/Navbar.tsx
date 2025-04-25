837404f5a77603be34c1c02ec89c8ad1f73e5ba6
Adithya Ajith <adithya@satyaloka.org>
2025-04-25 18:35:51 +0530

    Fix navbar sizing on mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex flex-col gap-2">
       <Separator />
-      <div className="w-full flex flex-row items-center gap-4 d-respira font-extrabold justify-evenly">
+      <div className="w-full flex flex-row items-center gap-4 d-respira font-extrabold justify-evenly text-[10px] md:text-lg">
         <p>HOME</p>
         <div className="h-10 border-r border-[var(--damask)]" />
         <p>ABOUT</p>
