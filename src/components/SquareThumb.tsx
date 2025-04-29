6df6ebdee858142a839c0347629b9bcaee7aa1e0
Adithya Ajith <adithya@satyaloka.org>
2025-04-29 12:59:51 -0500

    Resize images on smaller devices

--- src/components/SquareThumb.tsx
+++ src/components/SquareThumb.tsx
@@ -8,7 +8,7 @@ interface SquareThumbProps {
 
 export default function SquareThumb(props: SquareThumbProps) {
   return (
-    <div className={`w-full md:w-72 h-100 text-[var(--paper)] border-1 border-[var(--damask)] rounded-lg bg-[var(--red)] ${props.alt && "bg-[var(--secondary)]"}`}>
+    <div className={`w-full md:w-72 h-full md:h-100 text-[var(--paper)] border-1 border-[var(--damask)] rounded-lg bg-[var(--red)] ${props.alt && "bg-[var(--secondary)]"}`}>
       <div className="h-full p-4 flex flex-col gap-2">
         <img className={`w-full aspect-square object-cover rounded-lg border-1 ${props.alt && "border-[var(--damask)]" || "border-[var(--paper)]"}`} src={props.img} />
         <div className="h-full d-respira text-xl flex flex-row items-center justify-between">
