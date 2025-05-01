c49c82bddbac3814826ddc3e402efb224366a8ab
Adithya Ajith <adithya@satyaloka.org>
2025-05-01 10:17:26 -0500

    Adjust more sizing and spacing on mobile

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,6 +7,6 @@ interface ButtonProps {
 
 export default function Button(props: ButtonProps) {
   return (
-    <Link to={props.link} className="drop-shadow-lg text-sm md:text-lg w-fit hover:bg-[var(--damask)] hover:cursor-pointer transition-all border-1 border-[var(--damask)] px-2 rounded-lg bg-[var(--red)] d-respira active:scale-90 text-[var(--paper)]">{props.title}</Link>
+    <Link to={props.link} className="text-lg w-fit hover:bg-[var(--damask)] hover:cursor-pointer transition-all border-1 border-[var(--damask)] px-2 rounded-lg bg-[var(--red)] d-respira active:scale-90 text-[var(--paper)]">{props.title}</Link>
   )
 }
