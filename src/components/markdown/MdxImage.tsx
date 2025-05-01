4988941acaaab22490c702ce580088e595bf6a4e
Adithya Ajith <adithya@satyaloka.org>
2025-05-01 11:28:01 -0500

    Make image captions full width

--- src/components/markdown/MdxImage.tsx
+++ src/components/markdown/MdxImage.tsx
@@ -8,7 +8,7 @@ export default function MdxImage(props: MdxImageProps) {
   return (
     <div className="drop-shadow-2xl flex flex-col">
       <img className="border-1 border-[var(--damask)] rounded-bl-xl rounded-t-xl w-full object-contain" src={props.src} alt={props.alt} />
-      <div className="p-4 rounded-b-xl bg-[var(--damask)] text-[var(--paper)] border-1 border-[var(--damask)] self-end w-fit">
+      <div className="p-4 rounded-b-xl bg-[var(--damask)] text-[var(--paper)] border-1 border-[var(--damask)] text-end w-full">
         {props.alt}
       </div>
     </div>
