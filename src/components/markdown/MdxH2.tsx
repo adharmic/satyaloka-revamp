0f8c366b3b970d7d96d45d8947a5c2ea0c48b643
Adithya Ajith <adithya@satyaloka.org>
2025-05-01 13:39:56 -0500

    Remove unnecessary escape characters

--- src/components/markdown/MdxH2.tsx
+++ src/components/markdown/MdxH2.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 export default function MdxH2(props: PropsWithChildren) {
   return (
     <div className="font-[600] text-2xl border-0 border-[var(--damask)] w-fit rounded-lg">
-      {"\> "}{props.children}{" \<"}
+      {"> "}{props.children}{" <"}
     </div>
   )
 }
