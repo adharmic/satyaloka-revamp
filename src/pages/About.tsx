2ddf24f76a8e278a2c89da2baefc9c08e88cb94a
Adithya Ajith <adithya@satyaloka.org>
2025-05-01 21:30:28 -0500

    Add banner border

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
+import SectionHeader from "../components/SectionHeader.tsx";
+
 export default function About() {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
+      <SectionHeader title="About" />
+    <div>      
       This page is about Satyaloka! How meta.
     </div>
+  </div>
   )
 }
